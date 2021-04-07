94b2f4e9aea604572448ecf453f641c3f5207558
Eduardo <eduardosh1105@gmail.com>
2021-04-07 14:24:17 -0600

    Edited connect-four.js

--- connect-four.js
+++ connect-four.js
@@ -1,1 +1,22 @@
+import { Game } from "./games.js";
 
+let game = undefined;
+
+window.addEventListener("DOMContentLoaded", () => {
+	let playerOneInput = document.getElementById("player-1-name");
+	let playerTwoInput = document.getElementById("player-2-name");
+	let newGameButton = document.getElementById("new-game");
+
+	playerOneInput.addEventListener("keyup", newGameStatus);
+	playerTwoInput.addEventListener("keyup", newGameStatus);
+
+	function newGameStatus() {
+		let playerOneValue = document.getElementById("player-1-name").value;
+		let playerTwoValue = document.getElementById("player-2-name").value;
+		if (!playerOneValue && !playerTwoValue) {
+			newGameButton.disabled = false;
+		} else {
+			newGameButton.disabled = true;
+		}
+	}
+});
