2fc81ef1f4021641a9ab1db9bee616e09a9961cb
meagan <meagan.h.smith@gmail.com>
2021-04-07 16:27:05 -0500

    Complete phase two

--- connect-four.js
+++ connect-four.js
@@ -5,11 +5,20 @@ let game = undefined;
 function updateUI() {
 	let boardHolder = document.getElementById("board-holder");
 	let gameName = document.getElementById("game-name");
+    let clickTargets = document.getElementById("click-targets");
+
 	if (game === undefined) {
 		boardHolder.setAttribute("class", "is-invisible");
 	} else {
 		boardHolder.classList.remove("is-invisible");
 		gameName.innerHTML = game.getName();
+        if (game.currentPlayer === 2) {
+            clickTargets.classList.add("red");
+            clickTargets.classList.remove("black");
+        } else {
+            clickTargets.classList.add("black");
+            clickTargets.classList.remove("red");
+        }
 	}
 }
 
@@ -17,18 +26,15 @@ window.addEventListener("DOMContentLoaded", () => {
 	let playerOneInput = document.getElementById("player-1-name");
 	let playerTwoInput = document.getElementById("player-2-name");
 	let newGameButton = document.getElementById("new-game");
+    let clickTargets = document.getElementById("click-targets");
 
 	playerOneInput.addEventListener("keyup", newGameStatus);
 	playerTwoInput.addEventListener("keyup", newGameStatus);
-	newGameButton.addEventListener("click", () => {
-		let playerOneValue = document.getElementById("player-1-name").value;
-		let playerTwoValue = document.getElementById("player-2-name").value;
-		game = new Game(playerOneValue, playerTwoValue);
-		document.getElementById("player-1-name").value = "";
-		document.getElementById("player-2-name").value = "";
-		newGameStatus();
-		updateUI();
-	});
+	newGameButton.addEventListener("click", updateBoard);
+    clickTargets.addEventListener("click", () => {
+        game.playInColumn();
+        updateUI();
+    });
 
 	function newGameStatus() {
 		let playerOneValue = document.getElementById("player-1-name").value;
@@ -39,4 +45,14 @@ window.addEventListener("DOMContentLoaded", () => {
 			newGameButton.disabled = true;
 		}
 	}
+
+    function updateBoard() {
+        let playerOneValue = document.getElementById("player-1-name").value;
+		let playerTwoValue = document.getElementById("player-2-name").value;
+		game = new Game(playerOneValue, playerTwoValue);
+		document.getElementById("player-1-name").value = "";
+		document.getElementById("player-2-name").value = "";
+		newGameStatus();
+		updateUI();
+    }
 });
