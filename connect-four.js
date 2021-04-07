7515717e75326e03e703d74f8738fee421c3d884
Eduardo <eduardosh1105@gmail.com>
2021-04-07 16:02:09 -0600

    Added column.js

--- connect-four.js
+++ connect-four.js
@@ -5,20 +5,20 @@ let game = undefined;
 function updateUI() {
 	let boardHolder = document.getElementById("board-holder");
 	let gameName = document.getElementById("game-name");
-    let clickTargets = document.getElementById("click-targets");
+	let clickTargets = document.getElementById("click-targets");
 
 	if (game === undefined) {
 		boardHolder.setAttribute("class", "is-invisible");
 	} else {
 		boardHolder.classList.remove("is-invisible");
 		gameName.innerHTML = game.getName();
-        if (game.currentPlayer === 2) {
-            clickTargets.classList.add("red");
-            clickTargets.classList.remove("black");
-        } else {
-            clickTargets.classList.add("black");
-            clickTargets.classList.remove("red");
-        }
+		if (game.currentPlayer === 2) {
+			clickTargets.classList.add("red");
+			clickTargets.classList.remove("black");
+		} else {
+			clickTargets.classList.add("black");
+			clickTargets.classList.remove("red");
+		}
 	}
 }
 
@@ -26,15 +26,15 @@ window.addEventListener("DOMContentLoaded", () => {
 	let playerOneInput = document.getElementById("player-1-name");
 	let playerTwoInput = document.getElementById("player-2-name");
 	let newGameButton = document.getElementById("new-game");
-    let clickTargets = document.getElementById("click-targets");
+	let clickTargets = document.getElementById("click-targets");
 
 	playerOneInput.addEventListener("keyup", newGameStatus);
 	playerTwoInput.addEventListener("keyup", newGameStatus);
 	newGameButton.addEventListener("click", updateBoard);
-    clickTargets.addEventListener("click", () => {
-        game.playInColumn();
-        updateUI();
-    });
+	clickTargets.addEventListener("click", () => {
+		game.playInColumn();
+		updateUI();
+	});
 
 	function newGameStatus() {
 		let playerOneValue = document.getElementById("player-1-name").value;
@@ -46,13 +46,13 @@ window.addEventListener("DOMContentLoaded", () => {
 		}
 	}
 
-    function updateBoard() {
-        let playerOneValue = document.getElementById("player-1-name").value;
+	function updateBoard() {
+		let playerOneValue = document.getElementById("player-1-name").value;
 		let playerTwoValue = document.getElementById("player-2-name").value;
 		game = new Game(playerOneValue, playerTwoValue);
 		document.getElementById("player-1-name").value = "";
 		document.getElementById("player-2-name").value = "";
 		newGameStatus();
 		updateUI();
-    }
+	}
 });
